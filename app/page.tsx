097db0958a6b372c220cc2df24d9f557aabd06f8
Camilo Escar <camiloescar1995@gmail.com>
2025-11-26 14:12:44 -0300

    error ssr

--- app/page.tsx
+++ app/page.tsx
@@ -1,33 +1,37 @@
-import Image from "next/image"
+"use client";
 
-export default async function Page() {
+import { useEffect, useState } from "react";
 
-  const API = process.env.NEXT_PUBLIC_BACKEND_URL;
-  console.log("API:", API);
+export default function Page() {
+  const [productos, setProductos] = useState([]);
 
-  const response = await fetch(`${API}/productos`)
+  useEffect(() => {
+    async function load() {
+      try {
+        const res = await fetch(`${process.env.NEXT_PUBLIC_BACKEND_URL}/productos`);
+        const data = await res.json();
+        setProductos(data);
+      } catch (error) {
+        console.error("Error cargando productos:", error);
+      }
+    }
+    load();
+  }, []);
 
-  const productos = await response.json()
+  return (
+    <div>
+      <h1>Productos</h1>
 
-  console.log(productos)
-
-
-  return <div>
-    <h1>Productos</h1>
-
-    <div className="grid grid-cols-3 gap-4 p-20 container mx-auto">
-    {productos.map((producto:any) => (
-      <div className="bg-white p-5 text-black" key={producto.id}>
-        <h1>{producto.nombre}</h1>
-        <h3>{producto.precio}</h3>
-        <p>{producto.descripcion}</p>
-        <img src={producto.imagenURL} alt="" width={200} height={200}/>
-
-
+      <div className="grid grid-cols-3 gap-4 p-20 container mx-auto">
+        {productos.map((producto: any) => (
+          <div className="bg-white p-5 text-black" key={producto.id}>
+            <h1>{producto.nombre}</h1>
+            <h3>{producto.precio}</h3>
+            <p>{producto.descripcion}</p>
+            <img src={producto.imagenURL} width={200} height={200} alt="" />
+          </div>
+        ))}
       </div>
-    ))
-    
-  }
-  </div>
-  </div>
+    </div>
+  );
 }
